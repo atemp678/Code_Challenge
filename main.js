3896325d7a90c231c4f950a89ef39646e4a93d9d
atemp678 <ajt2158@columbia.edu>
2022-01-27 22:40:55 +0000

    first attempt at test suite

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ export function solution(input) {
   // logic here
   if (input > 1) {
     return input * 2;
-  }
-  if (text.matches("^[0-9]+$") && text.length() > 2) {
-    //...
+  } else {
     return null;
   }
 }
--- siblings.test.js
+++ siblings.test.js
@@ -1,7 +1,18 @@
+import each from "jest-each";
 import { solution } from "./main.js";
-const actual = solution(123);
-const expected = [321, 312, 231, 213, 132, 123];
+// const actual = solution(123);
+// const expected = [321, 312, 231, 213, 132, 123];
 
-if (actual !== expected) {
-  throw new Error(`${actual} value is not ${expected}`);
-}
+describe(solution, () => {
+  test.each`
+    input          | expectedResult
+    ${"abc"}       | ${undefined}
+    ${123}         | ${"321, 312, 231, 213, 132, 123"}
+    ${"A 3B2 C6D"} | ${"632, 623, 362, 326, 263, 236"}
+  `(`converts $input to $expectedResult`, ({ input, expectedResult }) => {
+    expect(solution(input)).toBe(expectedResult);
+  });
+});
+// if (actual !== expected) {
+//   throw new Error(`${actual} value is not ${expected}`);
+// }
