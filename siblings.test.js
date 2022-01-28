7a23b7eb546124c4901d31aeacff53cfc5d4ef8c
atemp678 <ajt2158@columbia.edu>
2022-01-28 11:39:49 +0000

    changed some syntax issues in text suite

--- siblings.test.js
+++ siblings.test.js
@@ -6,11 +6,11 @@ import { solution } from "./main.js";
 describe(solution, () => {
   test.each`
     input          | expectedResult
-    ${"abc"}       | ${undefined}
-    ${123}         | ${"321, 312, 231, 213, 132, 123"}
+    ${"abc"}       | ${"undefined"}
+    ${"123"}       | ${"321, 312, 231, 213, 132, 123"}
     ${"A 3B2 C6D"} | ${"632, 623, 362, 326, 263, 236"}
-    ${"1"}          |${"1"}
-    ${"0"}          |${"0"}
+    ${"1"}         | ${"1"}
+    ${"0"}         | ${"0"}
   `(`converts $input to $expectedResult`, ({ input, expectedResult }) => {
     expect(solution(input)).toBe(expectedResult);
   });
