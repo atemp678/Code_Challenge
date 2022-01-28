f3be7edda9543519c32b7e960e532ff02d682d0c
atemp678 <ajt2158@columbia.edu>
2022-01-28 12:01:53 +0000

    some corrections to the main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 export function solution(input) {
   // logic here
-  for (let i = 0; i > input.length; i++) {
-    for (let j = 1; j > input.length; j++) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 1; j < input.length; j++) {
       if (input[i] < input[j]) {
         let newPosition = swap(input[i], input[j]);
         return [...newPosition];
