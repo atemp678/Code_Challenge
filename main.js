a4788e4654c987155dcf974f8e9df9318c466236
atemp678 <ajt2158@columbia.edu>
2022-02-01 12:37:55 +0000

    updated strategy

--- main.js
+++ main.js
@@ -2,13 +2,31 @@
 //(refactor as time allows)
 
 ///Take in input, check if the input string is all integers and filter out spaces and letters.
-//if string has no errors return error
+//if string has no integers return error
 //make a new array which is the input string as only an integer
 //make sure 1 and 0 return 1 and 0 respectively
 //permutate the given integer string in order to give all ways the digits can be organized
 //make new array filtering out all duplicates
 //reorganize new array from ascending to descending numbers.
 
+//premutation:
+// 456= 654, 645, 564, 546, 465, 456
+// 4  = 4bc c4b bc4
+//5 = 5bc c5b bc5
+//6 = 6cb c6b cb6
+//xxx....xn;
+//for loop: input.length 
+//if the loop doesn't end call itself again (recursion of for loop?)
+
 export function solution(input) {
-  // logic here
-}
+    if (!input || typeof input !== "string"){
+        return "Please enter a string"
+      } else if (input.length<2) 
+      return input
+    }
+  let permutationsArray=[]
+  for (let i = 0; i < input.length; i++){
+    // do something
+  }
+  return permutationsArray
+};
